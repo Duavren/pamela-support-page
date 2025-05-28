73ba273fad6d141d36d078dc94fab6f1668fd8c9
Duavren <slsmina143@gmail.com>
2025-05-28 08:36:19 +0000

    New encouragement page with music and confetti

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
-// Welcome alerts
 alert("I'm proud of you, Pamela.");
 alert("Keep it up, bb! mwa!");
 
-// Play music when button is clicked
+// Play background music
 function playMusic() {
   const audio = document.getElementById("bg-music");
   audio.play();
 }
 
-// Send hug and launch confetti
+// Launch confetti on hug
 function sendHug() {
   alert("Sending virtual hug! 🤗");
-
   confetti({
-    particleCount: 200,
-    spread: 80,
+    particleCount: 180,
+    spread: 70,
     origin: { y: 0.6 }
   });
 }
